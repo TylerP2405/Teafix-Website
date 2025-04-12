9cc8519df54e332578462eb4fa35ead6e2598537
TylerP2405 <thinhpham2405@gmail.com>
2025-04-11 23:31:45 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        Hello World! This is Teafix website. This is a simple React app
+        created using Vite and TypeScript. This is a simple React app
+      </h1>
+    </div>
+  )
+}
+export default App;
